de59b3c28c2ee91e0f732e01ff11b8c53d1e946c
Roland <117606042+rcfisch@users.noreply.github.com>
2024-07-03 14:08:12 -0400

    Enemy

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -9,16 +9,16 @@ const gm = preload("res://Scripts/game_manager.gd")
 func _ready():
 	gm.enemy_list.append(self)
 	
-func _physics_process(delta):
-	if abs(gm.player_pos.x - global_position.x) + abs(gm.player_pos.y - global_position.y) < alert_distance:
-		alert()
+func _process(_delta):
+	if !alerted and abs(gm.player_pos.x - global_position.x) + abs(gm.player_pos.y - global_position.y) < alert_distance:
+		alert(true)
 	
-func alert(alert_others = true):
+func alert(alert_others):
 	alerted = true
+	print("Enemy Alerted")
 	if (!alert_others):
 		return
 	for i in gm.enemy_list.size():
-		
 		if abs(gm.enemy_list[i].global_position.x - global_position.x) + abs(gm.enemy_list[i].global_position.y - global_position.y) < alert_distance:
 			gm.enemy_list[i].get_script().alert(false)
 			
--- Scripts/WalkingEnemy.gd
+++ Scripts/WalkingEnemy.gd
@@ -10,10 +10,13 @@ var dir_to_player : int = 1
 func _physics_process(delta):
 	if !is_on_floor():
 		velocity.y += 1
+		
 	if alerted:
 		dir_to_player = clamp(gm.player_pos.x - global_position.x, -1, 1) 
+		scale.x = clamp(gm.player_pos.x - global_position.x, -1, 1)
 		if !check_for_obsticles():
 			move_toward_player()
+			
 	elif is_on_floor() and is_patrolling:
 		if check_for_obsticles():
 			dir = dir * -1
@@ -27,6 +30,6 @@ func move_toward_player():
 	velocity = Vector2(speed * dir_to_player, velocity.y)
 	
 func check_for_obsticles() -> bool:
-	if is_on_wall():
-		return true
+	if $RayCast2D2.is_colliding():
+		return false
 	return !$RayCast2D.is_colliding()
