cc3bf4fb8e70e564ff0c7a7cdd57736b0755ac20
Roland <117606042+rcfisch@users.noreply.github.com>
2024-07-06 22:08:27 -0400

    Added enemy health

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -3,7 +3,13 @@ class_name Enemy
 
 const gm = preload("res://Scripts/game_manager.gd")
 
+@export var can_be_alerted : bool
 @export var alert_distance : float
+
+@export var max_health : int
+@export var damage_dealt : int
+
+@onready var health : int = max_health
 var alerted = false
 
 func _ready():
@@ -12,17 +18,30 @@ func _ready():
 func _process(_delta):
 	if !alerted and abs(gm.player_pos.x - global_position.x) + abs(gm.player_pos.y - global_position.y) < alert_distance:
 		alert(true)
+	if $Killbox.get_overlapping_areas()
+		pass
 	
 func alert(alert_others):
+	if !can_be_alerted:
+		return	
 	alerted = true
-	print("Enemy Alerted")
 	if (!alert_others):
 		return
 	for i in gm.enemy_list.size():
 		if abs(gm.enemy_list[i].global_position.x - global_position.x) + abs(gm.enemy_list[i].global_position.y - global_position.y) < alert_distance:
 			pass
 			#gm.enemy_list[i].get_script().alert(false)
-			
+func player_hit():
+	pass
+func take_damage(damage):
+	health -= damage
+	if health <= 0:
+		on_death()
+		destory()
+	
+func on_death():
+	pass
+	
 func destory():
 	gm.enemy_list.erase(self)
 	self.queue_free()
